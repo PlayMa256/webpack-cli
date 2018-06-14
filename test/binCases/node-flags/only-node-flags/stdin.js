09057dd046590e449bc413c7f92b9c858f595e6a
PlayMa256 <matheus.g.silva@hotmail.com>
2018-06-14 12:24:52 -0300

    add test console log

--- test/binCases/node-flags/only-node-flags/stdin.js
+++ test/binCases/node-flags/only-node-flags/stdin.js
@@ -4,6 +4,10 @@ module.exports = function testAssertions(code, stdout, stderr) {
 	expect(code).toBe(0);
 	expect(stdout).toEqual(expect.anything());
 	expect(stdout[5]).toContain("main.js");
+	console.log(stdout[6])
+	console.log(stdout[7])
+	console.log(stdout[8])
+	console.log(stdout[9])
 	expect(stdout[7]).toMatch(/index\.js.*\{0\}/);
 	expect(stderr).toHaveLength(0);
 };
